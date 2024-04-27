96415bf300db3a02084693131e07739700836ac3
Pushpak Tanneeru <pushpak.tanneeru@calsoftinc.com>
2024-04-27 23:57:29 +0530

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@
 import './App.css'
 import Loginpage from './components/loginpage'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router , Routes, Route } from 'react-router-dom'
 import Signup from './components/signuppage'
 
 function App() {
 
   return (
-    <Router>
+    <Router basename='/codeketch'>
       <Routes>
-        <Route path='/codeketch' element={<Loginpage/>}/>
-        <Route path='/codeketch/signup' element={<Signup/>}/>
+        <Route path='/' element={<Loginpage/>} exact />
+        <Route  path='/signup' element={<Signup/>}/>
       </Routes>
     </Router>
   )
